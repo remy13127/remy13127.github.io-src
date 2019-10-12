be7f11439080ec59d5294d07ae77011b68312942
Remy Torro <remy.torro@etu.univ-amu.fr>
2019-10-12 17:05:02 +0200

    First commit.

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,7 +8,7 @@ SITEURL = ''
 
 PATH = 'content'
 PLUGIN_PATHS = ['pelican-plugins']
-PLUGINS = ['latex','pelican-cite','render_math']
+PLUGINS = ['latex','pelican-cite','render_math','better_figures_and_images']
 THEME = "/home/remy13127/pelican-themes/html5-dopetrope"
 
 TIMEZONE = 'Europe/Paris'
@@ -35,6 +35,6 @@ SOCIAL = (('You can add links in your config file', '#'),
 DEFAULT_PAGINATION = 10
 PUBLICATIONS_SRC = 'content/zotero.bib'
 MATH_JAX = {'linebreak_automatic':True,'responsive':True}
-
+RESPONSIVE_IMAGES = True
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
