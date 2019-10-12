e3f8f2b6a4256aa9a268743636ca2683912f2e2b
Remy Torro <remy.torro@etu.univ-amu.fr>
2019-10-12 21:21:40 +0200

    First commit.

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,17 +8,10 @@ SITEURL = ''
 
 PATH = 'content'
 PLUGIN_PATHS = ['pelican-plugins']
-PLUGINS = ['latex','pelican-cite','render_math','better_figures_and_images','tag_cloud']
+PLUGINS = ['latex','pelican-cite','render_math','better_figures_and_images']
 THEME = "/home/remy13127/pelican-themes/html5-dopetrope"
-SHARE_BUTTONS = ('twitter', 'facebook', 'whatsapp', 'mail')
+
 TIMEZONE = 'Europe/Paris'
-
-TAG_CLOUD = True
-TAG_CLOUD_STEPS = 5
-TAG_CLOUD_MAX_ITEMS = 50
-TAG_CLOUD_BADGE = True
-TAG_CLOUD_SORTING = 'size'
-
 
 DEFAULT_LANG = 'en'
 
@@ -29,19 +22,14 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#SOCIAL = (('You can add links in your config file', '#'),
+#          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 PUBLICATIONS_SRC = 'content/zotero.bib'
 MATH_JAX = {'linebreak_automatic':True,'responsive':True}
 RESPONSIVE_IMAGES = True
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
