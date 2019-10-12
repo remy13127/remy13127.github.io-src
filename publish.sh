b5f7bcba30a024ceb1732268b0ed07c54ae978db
Remy Torro <remy.torro@etu.univ-amu.fr>
2019-10-12 15:54:04 +0200

    First commit.

---
+++ publish.sh
@@ -0,0 +1,10 @@
+make content
+cd output
+git add .
+git commit -m "First post."
+git push -u origin master
+cd ..
+echo '*.pyc' >> .gitignore #don't need pyc files
+git add .
+git commit -m "First commit."
+git push -u origin master
