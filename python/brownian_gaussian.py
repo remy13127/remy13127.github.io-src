3f23a90b69a33a4b287f846aabb626929838aa59
Remy Torro <remy.torro@etu.univ-amu.fr>
2019-11-02 20:33:28 +0100

    First commit.

--- python/brownian_gaussian.py
+++ python/brownian_gaussian.py
@@ -6,13 +6,14 @@ N=1000
 D=0.5
 
 x = np.linspace(-5,5,1000)
-t = [0.1,0.2,0.5,2]
+t = [0.1,0.5,2,5]
 
 for time in t:
-	plt.plot(x,N/np.sqrt(4*pi*D*time)*np.exp(-x**2/(4*D*time)),label="t = "+str(time))
+	plt.plot(x,N/np.sqrt(4*pi*D*time)*np.exp(-x**2/(4*D*time)),label="t = "+str(time)+' s')
 
 plt.ylabel(r"f(x,t)")	
 plt.xlabel(r"x")
 plt.title("Diffusion of N = "+str(N)+" Brownian particles, with D = "+str(D))
+plt.spines['left'].set_position('zero')
 plt.legend()
-plt.savefig("/home/remy13127/Desktop/Website/content/images/brownian_gaussian.png")
+plt.savefig("/home/remy13127/Documents/ghpages/content/images/brownian_gaussian.png")
